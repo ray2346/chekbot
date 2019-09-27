d5883f54a768381cffe41db0850a99f85f9bdfb9
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-27 22:38:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const bot = new TelegramBot (TOKEN, {polling: true})
 bot.on('message', msg =>{
    bot.sendMessage(msg.chat.id,`Привет от chekbot: "Hi,🤝 ${msg.from.first_name}"`)
 })
+bot.on('message', msg =>{
+   bot.sendMessage(msg.chat.id,`${msg.from.first_name}, пожалуйста распишите данные:
+- тип справки (доктор, диагноз);
+- ФИО
+- дата рождения`)
+})
