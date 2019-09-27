2df6d4beb8b976db12d36bce1407a8839e8036c2
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-27 23:16:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const TelegramBot = require ('node-telegram-bot-api')
 
 const TOKEN = '857890236:AAHdpbmsUX7nMbOBmVVk0ubyW3mZZqxue08'
 const bot = new TelegramBot (TOKEN, {polling: true})
-bot.on('message', msg =>{
-   bot.sendMessage(msg.chat.id,`Привет от chekbot: "Hi,🤝 ${msg.from.first_name}"`)
-})
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply(''))
