fbda4716dda4c0ed1692677303f8e83484a23065
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-28 13:18:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const TelegramBot = require ('node-telegram-bot-api')
 const TOKEN = '857890236:AAHdpbmsUX7nMbOBmVVk0ubyW3mZZqxue08'
 const bot = new TelegramBot (TOKEN, {polling: true})
 
-bot.on('message', msg => {
-const { chat: { id }} = msg
-bot.sendMessage(id, 'Pong')
+bot.on('message', msg =>{
+   bot.sendMessage(msg.chat.id,`Привет от chekbot: "Hi,🤝 ${msg.from.first_name}"`)
 })
+
+bot.onText(/\/LOL (.+)/, msg => {
+const { chat: { id }} = msg
+bot.sendMessage(id, 'lil')
+})
+
+
+
+
+
+
+
 
