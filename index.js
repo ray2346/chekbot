bb76b278abe00cc564d3d7a06cad1bf56ab70e1d
ray2346 <54332835+ray2346@users.noreply.github.com>
2019-09-28 10:45:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const TelegramBot = require ('node-telegram-bot-api')
 const TOKEN = '857890236:AAHdpbmsUX7nMbOBmVVk0ubyW3mZZqxue08'
 const bot = new TelegramBot (TOKEN, {polling: true})
 bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
